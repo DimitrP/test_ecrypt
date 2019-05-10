fa4fb62b8fb3b8d41775007310bf53548752d0db
DmitryPersh <dmitrij.crimea@gmail.com>
2019-05-09 20:39:52 -0400

    AES Encryption Decryption

--- src/main/java/com/pershyn/ecrp/TestEncryptApplication.java
+++ src/main/java/com/pershyn/ecrp/TestEncryptApplication.java
@@ -1,5 +1,6 @@
 package com.pershyn.ecrp;
 
+import com.pershyn.ecrp.model.AES;
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -11,7 +12,14 @@ public class TestEncryptApplication implements CommandLineRunner {
 
     @Value("${app.password}")
     private String string;
+
+    @Value("${app.password2}")
+    private String string2;
+
+
     private String str = "SomeSecretCombination";
+
+//    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
 
@@ -31,6 +39,19 @@ public class TestEncryptApplication implements CommandLineRunner {
         String str3 = textEncryptor.decrypt(string);
 
         System.out.println("\ndecrypted password: "+str3);
+
+
+
+        System.out.println("Example 2:");
+
+        String encryptedString = AES.encrypt(string2, "secretKey") ;
+        String decryptedString = AES.decrypt(encryptedString, "secretKey") ;
+
+        System.out.println(string2);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+
+
 
 
     }
